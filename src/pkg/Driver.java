38129b193f3a3d976bb9654b3fc2f620a81f2cb0
amanda <amanda.hughez@gmail.com>
2014-09-30 17:07:47 +0100

    Created gui class

---
+++ src/pkg/Driver.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Gui gui = new Gui();
+		gui.setupGui();
+
+	}
+
+}
---
+++ src/pkg/Gui.java
@@ -0,0 +1,29 @@
+package pkg;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Gui extends JFrame {
+	
+	private JButton button1 = new JButton();
+	private JButton button2 = new JButton();
+	private JButton button3 = new JButton();
+	private JButton button4 = new JButton();
+	private JButton button5 = new JButton();
+	private JButton button6 = new JButton();
+	private JButton button7 = new JButton();
+	private JButton button8 = new JButton();
+	private JButton button9 = new JButton();
+	
+	public void setupGui(){
+
+	
+
+	
+	this.setSize(300,300);
+	this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+	this.setVisible(true);
+	}
+
+}
